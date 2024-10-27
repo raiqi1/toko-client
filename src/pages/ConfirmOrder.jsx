aba8fa924101f54b03253ddd5d59126e3b3bfa31
rayiqi <ramadifan769@gmail.com>
2024-10-28 05:32:38 +0700

    initial

--- src/pages/ConfirmOrder.jsx
+++ src/pages/ConfirmOrder.jsx
@@ -55,7 +55,7 @@ const ConfirmOrder = () => {
     const orderId = localStorage.getItem("orderId");
     if (orderId) {
       try {
-        await axios.get(`http://localhost:5000/api/order/confirm/${orderId}`);
+        await axios.get(`https://ecom-api-swart.vercel.app/api/order/confirm/${orderId}`);
         localStorage.removeItem("orderId");
         setLoader(false);
       } catch (error) {
